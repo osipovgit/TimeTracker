8435ac2c91983376832633f80020f6d891c56857
osipovgit <osipovs130@gmail.com>
2020-07-29 18:54:35 +0700

    Init commit

---
+++ src/main/java/ru/eltex/Application.java
@@ -0,0 +1,18 @@
+package ru.eltex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Класс для запуска программы
+ *
+ * @author Evgeni Osipov
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
